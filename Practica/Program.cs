aef25c3c0f62eee9e46eac00f91935f0d39484d1
FK-LIBREDELL\Brandel Otero <bdotero@finaktiva.com>
2022-06-06 13:44:34 -0500

    Proyecto base

---
+++ Practica/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
